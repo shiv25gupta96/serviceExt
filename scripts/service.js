d208714ca27942a0207bc55b6e6abda2480b7bd8
shiv25gupta96 <shivam25gupta96@gmail.com>
2018-08-15 23:53:15 +0530

    Mail Basic Completed. this is not advanced

--- scripts/service.js
+++ scripts/service.js
@@ -12,5 +12,26 @@
         jQuery('body').append('<div class="my-ext-menu">\n\
                 <a href="#" class="send-mail"></a>\n\
             </div>');
+        jQuery('.send-mail').click(function (e) { 
+            jQuery('.send-mail').attr("href", 'mailto:'+formMessage());
+        }); 
+        
+    }
+
+    function formMessage(){
+        var caller_id = jQuery('#incident\\.caller_id_label').val();
+        var opened_by = jQuery('#incident\\.opened_by_label').val();
+        var incident_number = jQuery('#sys_readonly\\.incident\\.number').val();
+        var follow_up = jQuery('#incident\\.u_sub_pending_reason').val();
+        var short_decription = jQuery('#incident\\.short_description').val();
+        var description = jQuery('#activity-stream-comments-textarea').val();
+
+        return encodeURI(caller_id + '?cc=MDA_Mango_Support,' + opened_by + '&subject=' + short_decription + '&body=' + description);
+    }
+
+    function showMailButton(){
+        jQuery('.send-mail').click(function (e) { 
+            jQuery('.send-mail').attr("href", 'mailto:'+formMessage());
+        }); 
     }
 })();
