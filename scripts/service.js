6ff7df6d3b1260c3ae0b5e36aae7787fb88bccc9
shiv25gupta96 <shivam25gupta96@gmail.com>
2018-08-16 11:17:28 +0530

    Fixed an error

--- scripts/service.js
+++ scripts/service.js
@@ -26,7 +26,7 @@
         var short_decription = jQuery('#incident\\.short_description').val();
         var description = jQuery('#activity-stream-comments-textarea').val();
 
-        return encodeURI(caller_id + '?cc=MDA_ManGo-Support,' + opened_by + '&subject=' + short_decription + '&body=' + description);
+        return encodeURI(caller_id + '?cc=MDA_ManGo-Support,' + opened_by + '&subject='+ [(follow_up.search('/Follow/')>-1) ? ('Reminder ' + follow_up[follow_up.length - 1] + ' :: ') : ''] + short_decription + '&body=' + description);
     }
 
     function showMailButton(){
