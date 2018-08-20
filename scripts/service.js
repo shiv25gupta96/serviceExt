b6c5baab7d168548deca069586d58a1d4195e96c
Gupta <shivam.p.gupta@accenture.com>
2018-08-20 22:45:15 +0530

    sam1

--- scripts/service.js
+++ scripts/service.js
@@ -13,13 +13,33 @@
     }
     var $ = jQuery;
     //Form table data
-    var form = window.g_form | document.querySelector('#gsft_main').contentWindow.g_form;
+    var form = window.g_form || (document.querySelector('#gsft_main') ? document.querySelector('#gsft_main').contentWindow.g_form : {});
 
     if(form){
        showMailButton();        
     }
 
     function incident_formMessage(){
+
+        var gf = new GlideRecord(form.tableName);
+        gf.get(form.getUniqueValue());
+
+        var caller_id = $('#incident\\.caller_id_label').val();
+        var opened_by = $('#incident\\.opened_by_label').val();
+        var ticket_number = $('#sys_readonly\\.incident\\.number').val();
+        var follow_up = $('#incident\\.u_sub_pending_reason').val();
+        var short_decription = $('#incident\\.short_description').val();
+        if(short_decription.search("::")>-1){
+            short_decription = short_decription.append(short_decription.match(/mango/i)[0].length, ticket_number);
+        } 
+        else
+            short_decription = "Regarding ManGo ticket " + ticket_number;
+        var description = $('#activity-stream-comments-textarea').val();
+
+        return encodeURI(caller_id + '?cc=MDA_ManGo-Support,' + opened_by + '&subject='+ [(follow_up.search(/Follow/)>-1) ? ('Reminder ' + follow_up[follow_up.length - 1] + ' :: ') : ''] + short_decription + '&body=' + description);
+    }
+
+    function sc_task_formMessage(){
         var caller_id = $('#incident\\.caller_id_label').val();
         var opened_by = $('#incident\\.opened_by_label').val();
         var ticket_number = $('#sys_readonly\\.incident\\.number').val();
