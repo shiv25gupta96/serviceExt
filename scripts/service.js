13883e194b618cf3612409e5bb480fd7cc6208c4
Gupta <shivam.p.gupta@accenture.com>
2018-08-20 22:27:49 +0530

    sample

--- scripts/service.js
+++ scripts/service.js
@@ -12,37 +12,38 @@
         return this.slice(0, ind) + ' ' + data + this.slice(ind + 1);
     }
     var $ = jQuery;
-    var loc = document.location.pathname;
-    var gsft_loc = $('#gsft_main');
-    if(document.location.pathname == '/sc_task.do' || document.location.pathname == '/incident.do'){
-        jQuery('body').append('<div class="my-ext-menu">\n\
-                <a href="#" class="send-mail"></a>\n\
-            </div>');
-        jQuery('.send-mail').click(function (e) { 
-            jQuery('.send-mail').attr("href", 'mailto:'+formMessage());
-        }); 
-        
+    //Form table data
+    var form = window.g_form | document.querySelector('#gsft_main').contentWindow.g_form;
+
+    if(form){
+       showMailButton();        
     }
 
-    function formMessage(){
-        var caller_id = jQuery('#incident\\.caller_id_label').val();
-        var opened_by = jQuery('#incident\\.opened_by_label').val();
-        var ticket_number = jQuery('#sys_readonly\\.incident\\.number').val();
-        var follow_up = jQuery('#incident\\.u_sub_pending_reason').val();
-        var short_decription = jQuery('#incident\\.short_description').val();
+    function incident_formMessage(){
+        var caller_id = $('#incident\\.caller_id_label').val();
+        var opened_by = $('#incident\\.opened_by_label').val();
+        var ticket_number = $('#sys_readonly\\.incident\\.number').val();
+        var follow_up = $('#incident\\.u_sub_pending_reason').val();
+        var short_decription = $('#incident\\.short_description').val();
         if(short_decription.search("::")>-1){
             short_decription = short_decription.append(short_decription.match(/mango/i)[0].length, ticket_number);
         } 
         else
             short_decription = "Regarding ManGo ticket " + ticket_number;
-        var description = jQuery('#activity-stream-comments-textarea').val();
+        var description = $('#activity-stream-comments-textarea').val();
 
         return encodeURI(caller_id + '?cc=MDA_ManGo-Support,' + opened_by + '&subject='+ [(follow_up.search(/Follow/)>-1) ? ('Reminder ' + follow_up[follow_up.length - 1] + ' :: ') : ''] + short_decription + '&body=' + description);
     }
 
     function showMailButton(){
-        jQuery('.send-mail').click(function (e) { 
-            jQuery('.send-mail').attr("href", 'mailto:'+formMessage());
+
+        $('body').append('<div class="my-ext-menu">\n\
+                <a href="#" class="send-mail"></a>\n\
+            </div>');
+
+        $('.send-mail').click(function (e) { 
+            messageFun = form.tableName + '_formMessage';
+            $('.send-mail').attr("href", 'mailto:' + messageFun());
         }); 
     }
 })();
